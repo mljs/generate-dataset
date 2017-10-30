b1bc8a8140af1f2d8b7b8157499fc6d7bb770888
jobo322 <jose.bolanos@correounivalle.edu.co>
2017-10-30 10:43:30 +0100

    documentation of main function and example

--- src/index.js
+++ src/index.js
@@ -1,6 +1,31 @@
+
 var {Matrix} = require('ml-matrix');
 var normalRandGenerator = require('distributions-normal-random');
 
+/**
+ * Create a dataset by the linear combination of pure elements with a specific gaussian distribution,
+ * @param {Array<Array<number>>} pureElements - matrix where each row is a pure element
+ * @param {object} options
+ * @param {boolean} options.keepCompositionMatrix - if it's true, the result object contain the composition matrix -compositionMatrix-
+ * @param {boolean} options.keepDataClass - if it's true, the result object contain the dataClass matrix.
+ * @param {boolean} options.dummyMatrix - if it's true, the dataClass matrix will be a dummyMatrix other wize it's a vector matrix.
+ * @param {number} options.seed - the seed to initialize the random vector.
+ * @param {Array<object>} options.classes - the parameters to create the dataset. see the example.
+ * @param {number} options.classes[*].nbSample - number of sample for this class.
+ * @param {Array<object>} elements - contain the object with the index of pure element and the distribution parameters. see the example.
+ * {
+ *       index: 0,
+ *       distribution: {
+ *           name: 'gaussian',
+ *           parameters: {
+ *               mean: 9.4,
+ *               standardDesviation: 0.1
+ *           }
+ *       }
+ *  }
+ * @return {object} - object with one or three properties depending on the options
+ * {dataset: Array<Array<number>>, compositionMatrix: Array<Array<number>>, dataClass: Array<Array<number>>}.
+ */
 function generator(pureElements, options = {}) {
     _checkParameters(pureElements, options);
     var compositionMatrix = _createCompMatrix(options);
