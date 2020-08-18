73904e2b3143a1d13686a1918835320586e25e66
jobo322 <jose.bolanos@correounivalle.edu.co>
2020-08-18 11:32:00 -0500

    feat: add gcms type simulation

--- src/__tests__/test.js
+++ src/__tests__/test.js
@@ -1,101 +1,99 @@
-'use strict';
+import { generate } from '../index';
 
-var generate = require('../index');
-
-var options = {
-    seed: 22,
-    classes: [
+let options = {
+  seed: 22,
+  classes: [
+    {
+      nbSpectra: 500,
+      unitParameters: [
         {
-            nbSpectra: 500,
-            unitParameters: [
-                {
-                    index: 0,
-                    distribution: {
-                        name: 'normal',
-                        parameters: {
-                            mean: 9.4,
-                            standardDeviation: 0.1
-                        }
-                    }
-                },
-                {
-                    index: 1,
-                    distribution: {
-                        name: 'normal',
-                        parameters: {
-                            mean: 9.4,
-                            standardDeviation: 0.1
-                        }
-                    }
-                },
-                {
-                    index: 2,
-                    distribution: {
-                        name: 'normal',
-                        parameters: {
-                            mean: 9.4,
-                            standardDeviation: 0.1
-                        }
-                    }
-                }
-            ]
+          index: 0,
+          distribution: {
+            name: 'normal',
+            parameters: {
+              mean: 9.4,
+              standardDeviation: 0.1,
+            },
+          },
         },
         {
-            nbSpectra: 500,
-            unitParameters: [
-                {
-                    index: 0,
-                    distribution: {
-                        name: 'normal',
-                        parameters: {
-                            mean: 9.4,
-                            standardDeviation: 0.1
-                        }
-                    }
-                },
-                {
-                    index: 1,
-                    distribution: {
-                        name: 'normal',
-                        parameters: {
-                            mean: 10.3,
-                            standardDeviation: 0.15
-                        }
-                    }
-                },
-                {
-                    index: 2,
-                    distribution: {
-                        name: 'normal',
-                        parameters: {
-                            mean: 9.4,
-                            standardDeviation: 0.1
-                        }
-                    }
-                }
-            ]
-        }
-    ]
+          index: 1,
+          distribution: {
+            name: 'normal',
+            parameters: {
+              mean: 9.4,
+              standardDeviation: 0.1,
+            },
+          },
+        },
+        {
+          index: 2,
+          distribution: {
+            name: 'normal',
+            parameters: {
+              mean: 9.4,
+              standardDeviation: 0.1,
+            },
+          },
+        },
+      ],
+    },
+    {
+      nbSpectra: 500,
+      unitParameters: [
+        {
+          index: 0,
+          distribution: {
+            name: 'normal',
+            parameters: {
+              mean: 9.4,
+              standardDeviation: 0.1,
+            },
+          },
+        },
+        {
+          index: 1,
+          distribution: {
+            name: 'normal',
+            parameters: {
+              mean: 10.3,
+              standardDeviation: 0.15,
+            },
+          },
+        },
+        {
+          index: 2,
+          distribution: {
+            name: 'normal',
+            parameters: {
+              mean: 9.4,
+              standardDeviation: 0.1,
+            },
+          },
+        },
+      ],
+    },
+  ],
 };
 describe('dataset generated from a small matrix of pureElements', () => {
-    let pureElements = [
-        [0, 0, 0, 1, 0, 0, 0],
-        [0, 0, 0, 0, 0, 1, 0],
-        [0, 1, 0, 0, 0, 0, 0]
-    ];
-    let result = generate(pureElements, options);
-    it('the marker should has a mean closest to provided', () => {
-        let column = result.dataset.getColumn(5);
-        let [class1, class2] = [column.slice(0, 500), column.slice(500, 1000)];
-        let sum1 = class1.reduce((a, b) => a + b, 0);
-        let sum2 = class2.reduce((a, b) => a + b, 0);
-        expect(sum1 / class1.length).toBeCloseTo(9.4, 1);
-        expect(sum2 / class2.length).toBeCloseTo(10.3, 1);
-    });
-    it('the classMatrix should be a binary matrix and classVector contain the same information', () => {
-        expect(result.classVector[0]).toEqual(0);
-        expect(result.classVector[500]).toEqual(1);
-        expect(result.classMatrix.getRow(0)).toEqual([1, 0]);
-        expect(result.classMatrix.getRow(500)).toEqual([0, 1]);
-    });
+  let pureElements = [
+    [0, 0, 0, 1, 0, 0, 0],
+    [0, 0, 0, 0, 0, 1, 0],
+    [0, 1, 0, 0, 0, 0, 0],
+  ];
+  let result = generate(pureElements, options);
+  it('the marker should has a mean closest to provided', () => {
+    let column = result.dataset.getColumn(5);
+    let [class1, class2] = [column.slice(0, 500), column.slice(500, 1000)];
+    let sum1 = class1.reduce((a, b) => a + b, 0);
+    let sum2 = class2.reduce((a, b) => a + b, 0);
+    expect(sum1 / class1.length).toBeCloseTo(9.4, 1);
+    expect(sum2 / class2.length).toBeCloseTo(10.3, 1);
+  });
+  it('the classMatrix should be a binary matrix and classVector contain the same information', () => {
+    expect(result.classVector[0]).toEqual(0);
+    expect(result.classVector[500]).toEqual(1);
+    expect(result.classMatrix.getRow(0)).toEqual([1, 0]);
+    expect(result.classMatrix.getRow(500)).toEqual([0, 1]);
+  });
 });
--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
-'use strict';
-
-var {Matrix} = require('ml-matrix');
-var normalRandGenerator = require('distributions-normal-random');
+import normalRandGenerator from 'distributions-normal-random';
+import { Matrix } from 'ml-matrix';
 
 /**
  * Create a dataset by the linear combination of pure elements with a specific gaussian distribution,
@@ -14,75 +12,175 @@ var normalRandGenerator = require('distributions-normal-random');
  * @return {object} - object with one or three properties depending on the options
  * {dataset: Array<Array<number>>, compositionMatrix: Array<Array<number>>, dataClass: Array<Array<number>>}.
  */
-function generate(pureElements, options = {}) {
-    checkParameters(pureElements, options);
-    var compositionMatrix = createCompMatrix(options);
-    let [classVector, classMatrix] = getDataClass(options);
-    return {
-        compositionMatrix,
-        dataset: compositionMatrix.mmul(pureElements),
-        classVector,
-        classMatrix
-    };
+export function generate(pureElements, options = {}) {
+  let { type = 'general' } = options;
+  checkParameters(pureElements, options);
+  let result = {};
+  let creatorCompMatrix;
+  if (type === 'gcms') {
+    let times = getTimes(options.classes);
+    creatorCompMatrix = gcMsCompMatrix;
+    result.times = times;
+    options.times = times;
+  } else {
+    creatorCompMatrix = createCompMatrix;
+  }
+  let compositionMatrix = creatorCompMatrix(options);
+  let [classVector, classMatrix] = getDataClass(options);
+  return Object.assign(
+    {
+      compositionMatrix,
+      dataset: compositionMatrix.mmul(pureElements),
+      classVector,
+      classMatrix,
+    },
+    result,
+  );
 }
 
 function getDataClass(options) {
-    let counter = 0;
-    let classes = options.classes;
-    let nbClasses = classes.length;
-    let nbSamples = classes.reduce((acc, clase) => clase.nbSpectra + acc, 0);
+  let counter = 0;
+  let classes = options.classes;
+  let nbClasses = classes.length;
+  let nbSamples = classes.reduce((acc, clase) => clase.nbSpectra + acc, 0);
 
-    let classVector = new Array(nbSamples);
-    let classMatrix = Matrix.zeros(nbSamples, nbClasses);
-    for (let i = 0; i < nbClasses; i++) {
-        let nbSamplesPerClass = classes[i].nbSpectra;
-        for (let j = 0; j < nbSamplesPerClass; j++) {
-            classMatrix.set(counter, i, 1);
-            classVector[counter++] = i;
-        }
+  let classVector = new Array(nbSamples);
+  let classMatrix = Matrix.zeros(nbSamples, nbClasses);
+  for (let i = 0; i < nbClasses; i++) {
+    let nbSamplesPerClass = classes[i].nbSpectra;
+    for (let j = 0; j < nbSamplesPerClass; j++) {
+      classMatrix.set(counter, i, 1);
+      classVector[counter++] = i;
     }
+  }
 
-    return [classVector, classMatrix];
+  return [classVector, classMatrix];
 }
 
 function createCompMatrix(options) {
-    var {
-        classes,
-        nbPureElements,
-        seed = Date.now()
-    } = options;
-    normalRandGenerator.seed = seed;
-    let matrixComposition = [];
-    for (let classParameters of classes) {
-        for (let i = 0; i < classParameters.nbSpectra; i++) {
-            let arrayComposition = new Array(nbPureElements).fill(0);
-            for (let element of classParameters.unitParameters) {
-                let mean = element.distribution.parameters.mean;
-                let std = element.distribution.parameters.standardDeviation;
-                arrayComposition[element.index] = ensureNonNegativeComposition(mean, std);
-            }
-            matrixComposition.push(arrayComposition);
-        }
+  let { classes, nbPureElements, seed = Date.now() } = options;
+  normalRandGenerator.seed = seed;
+  let matrixComposition = [];
+  for (let classParameters of classes) {
+    for (let i = 0; i < classParameters.nbSpectra; i++) {
+      let arrayComposition = new Array(nbPureElements).fill(0);
+      for (let element of classParameters.unitParameters) {
+        let mean = element.distribution.parameters.mean;
+        let std = element.distribution.parameters.standardDeviation;
+        arrayComposition[element.index] = ensureNonNegativeComposition(
+          mean,
+          std,
+        );
+      }
+      matrixComposition.push(arrayComposition);
     }
-    return new Matrix(matrixComposition);
+  }
+  return new Matrix(matrixComposition);
+}
+
+function gcMsCompMatrix(options) {
+  let { classes, nbPureElements, times } = options;
+
+  let matrixComposition = Matrix.zeros(times.length, nbPureElements);
+  let sqrt2Pi = Math.sqrt(Math.PI * 2);
+  for (let classParameters of classes) {
+    for (let element of classParameters.unitParameters) {
+      let intensity = element.distribution.parameters.intensity;
+      let mean = element.distribution.parameters.mean;
+      let std = element.distribution.parameters.standardDeviation;
+      let factor = intensity / std / sqrt2Pi;
+      let func = (t) => factor * Math.exp(-0.5 * Math.pow((t - mean) / std, 2));
+      for (let i = 0; i < times.length; i++) {
+        let value = func(times[i]) + matrixComposition.get(i, element.index);
+        matrixComposition.set(i, element.index, value);
+      }
+    }
+  }
+  return matrixComposition;
+}
+
+function getMinMaxIndexRT(classes) {
+  let max = Number.MIN_SAFE_INTEGER;
+  let min = Number.MAX_SAFE_INTEGER;
+  let minIndexs = [0, 0];
+  let maxIndexs = [0, 0];
+  for (let i = 0; i < classes.length; i++) {
+    let classParameters = classes[i];
+    let unitParameters = classParameters.unitParameters;
+    for (let j = 0; j < unitParameters.length; j++) {
+      let element = unitParameters[j].distribution.parameters;
+      if (max < element.mean) {
+        max = element.mean;
+        maxIndexs = [i, j];
+      }
+      if (min > element.mean) {
+        min = element.mean;
+        minIndexs = [i, j];
+      }
+    }
+  }
+  return { minIndexs, maxIndexs };
+}
+
+function getTimes(classes) {
+  let { minIndexs, maxIndexs } = getMinMaxIndexRT(classes);
+
+  let fromElement =
+    classes[minIndexs[0]].unitParameters[minIndexs[1]].distribution.parameters;
+  let toElement =
+    classes[maxIndexs[0]].unitParameters[maxIndexs[1]].distribution.parameters;
+
+  let from = fromElement.mean - fromElement.standardDeviation * 5;
+  let to = toElement.mean + toElement.standardDeviation * 5;
+
+  let nbPoints = classes.reduce((a, b) => a + b.nbSpectra, 0);
+  let jump = (to - from) / (nbPoints - 1);
+
+  let times = new Float32Array(nbPoints);
+
+  for (let i = 0; i < nbPoints; i++) {
+    times[i] = from + i * jump;
+  }
+
+  return times;
 }
 
 function checkParameters(pureElements, options) {
-    if (!Array.isArray(pureElements)) throw new RangeError('pureElements should be an Array');
-    if (pureElements.length < 2) throw new RangeError('pureElements array should has more than one element');
-    if (!Array.isArray(options.classes)) throw new RangeError('classes should be an Array');
-    options.nbPureElements = pureElements.length;
+  if (!Array.isArray(pureElements)) {
+    throw new RangeError('pureElements should be an Array');
+  }
+  if (pureElements.length < 2) {
+    throw new RangeError('pureElements array should has more than one element');
+  }
+  if (!Array.isArray(options.classes)) {
+    throw new RangeError('classes should be an Array');
+  }
+
+  let pureIndexs = new Set();
+  let classes = options.classes;
+  for (let i = 0; i < classes.length; i++) {
+    let classParameters = classes[i];
+    let unitParameters = classParameters.unitParameters;
+    for (let j = 0; j < unitParameters.length; j++) {
+      pureIndexs.add(unitParameters[j].index);
+    }
+  }
+  let nbPureElements = Array.from(pureIndexs).length;
+  if (nbPureElements !== pureElements.length) {
+    throw new RangeError(
+      'the number of pureElement should be equal to the number of called pure indexs',
+    );
+  }
+  options.nbPureElements = nbPureElements;
 }
 
 function ensureNonNegativeComposition(mean, std) {
-    if (mean < 0) throw new Error('mean is negative');
-    let composition;
-    let isNegative = true;
-    while(isNegative) {
-        composition = normalRandGenerator() * std + mean;
-        if (composition >= 0) isNegative = false;
-    }
-    return composition;
+  if (mean < 0) throw new Error('mean is negative');
+  let composition;
+  let isNegative = true;
+  while (isNegative) {
+    composition = normalRandGenerator() * std + mean;
+    if (composition >= 0) isNegative = false;
+  }
+  return composition;
 }
-
-module.exports = generate;
